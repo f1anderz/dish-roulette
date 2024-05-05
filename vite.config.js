24acb99be6cf021bc11ae7c4d71ec4f4cd6a3fa1
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-05-05 21:44:08 +0300

    added base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import {fileURLToPath, URL} from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/dish-roulette/',
     plugins: [react()], resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
